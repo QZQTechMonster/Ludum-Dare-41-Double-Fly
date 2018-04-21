7fc43e773f2f400d0a79f4a6e5f486cfcf693489
QZQTechMonster <928221136@qq.com>
2018-04-21 11:52:09 +0800

    add tutorial

---
+++ Assets/_Core/GameSpeedController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameSpeedController : MonoBehaviour {
+
+    [SerializeField] float gameSpeed = 1;
+    [SerializeField] Text tutorialText;
+	[SerializeField] string tutorialString;
+
+    bool isStop = false;
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if(other.gameObject.tag != "Player") return;
+        print("trig");
+        tutorialText.text = tutorialString;
+        Time.timeScale = gameSpeed;
+        isStop = true;
+    }
+
+
+	void Update() {
+        if (isStop && Input.GetKeyDown(KeyCode.Space)) {
+            print("Des");
+            Time.timeScale = 1;
+            tutorialText.text = "";
+            Destroy(gameObject);
+        }
+	}
+
+}
